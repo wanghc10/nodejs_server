1fbdc60b6f7697fb59c7ac3149d9237d42bc0c38
root <wanghc10@163.com>
2016-02-23 09:45:02 +0000

    base get/post but bugs exist

--- app.js
+++ app.js
@@ -1,31 +1,89 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-var path = require("path");
-
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var session = require('express-session');
 var port = 8000;
-// var port = process.env.PORT || 8000;
 
 app.set('views', './views');
- 
-app.set( 'view engine', 'html' );
+app.set('view engine', 'html' );
+// app.engine('.html', require('ejs').express );
 app.engine( '.html', require( 'ejs' ).__express );
- 
-app.get('/', function(req, res) {
-    res.render('index');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer());
+app.use(session({
+    secret:'secret',
+    resave:true,
+    saveUninitialized:false,
+    cookie:{
+        maxAge:1000*60*10  //过期时间设置(单位毫秒)
+    }
+}));
+app.get('/',function(req,res){
+    res.render('login');
 });
-    
+
 app.get('/login',function(req,res){
     res.render('login');
 });
- 
-app.get('/home',function(req,res){
-    res.render('home');
-});
-// app.get('/index.html', function (req, res) {
-//     res.redirect("http://www.baidu.com");   //重定向网页
-// //   res.sendFile( __dirname + "/views/" + "index.html" );
-// })
 
+app.use(function(req, res, next){
+    res.locals.user = req.session.user;
+    var err = req.session.error;
+    res.locals.message = '';
+    if (err) res.locals.message = '<div style="margin-bottom: 20px;color:red;">' + err + '</div>';
+    next();
+});
+
+app.post('/login',function(req,res){
+    var user={
+        username:'admin',
+        password:'admin'
+    }
+ if(req.body.username==user.username&&req.body.password==user.password){
+        req.session.user = user;
+        res.send(200);
+    }else{
+        req.session.error = "用户名或密码不正确";
+        res.send( 404 );
+    }
+});
+
+// app.post("/login", function(req, res) {
+//     console.log("用户名称为：" + req.body.username);
+//     console.log("用户密码为：" + req.body.password);
+//     var user={
+//         username:'admin',
+//         password:'admin'
+//     }
+    
+//     if((req.body.username==user.username)&&(req.body.password==user.password)){
+//       console.log("login ok");
+//       res.send(200);
+//     }else{
+//       console.log("login err");
+//       res.send(404);
+//     }
+// });
+
+app.get('/logout', function(req, res){
+    req.session.user = null;
+    req.session.error = null;
+    res.redirect('index');
+});
+app.get('/index', function(req, res) {
+    res.render('index');
+});
+
+app.get('/home',function(req,res){
+    if(req.session.user){
+        res.render('home');
+    }else{
+        req.session.error = "请先登录"
+        res.redirect('login');
+    }
+});
 var server = app.listen(port, function(){
     var host = server.address().address;
     var post = server.address().port;
